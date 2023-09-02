20c73ebc1a569bde52018a2aa3e333cab87a3fba
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2023-09-02 10:58:04 +0200

    pass gpio to test

--- test/testdhtxx.cpp
+++ test/testdhtxx.cpp
@@ -10,15 +10,18 @@ int main(int argc, char** argv)
 
     auto a=QCoreApplication(argc, argv);
 
+    quint8 gpio=4;
+    if(argc>1)gpio=atoi(argv[1]);    
+    
     QTimer *timer = new QTimer(&a);
-    auto sensor=dhtxx(4,&a);
+    auto sensor=dhtxx(gpio,&a);
 
     QObject::connect(&sensor, &dhtxx::got_reading,&a, [&sensor](){
         qDebug()<<"Temperature:"<<sensor.temp();
         qDebug()<<"Humidity:"<<sensor.humi();
     });
     QObject::connect(timer, &QTimer::timeout, &sensor, &dhtxx::read);
-    timer->start(1000);
+    timer->start(3000);
 
     return a.exec();
 }
